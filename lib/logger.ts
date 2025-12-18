2a7e28b1d4daa0cf422f194db06160de190ba8d3
Alex Spinu <spinualexandru@outlook.com>
2025-12-18 09:41:41 +0200

    chore: logger

--- lib/logger.ts
+++ lib/logger.ts
@@ -1,20 +1,20 @@
 import { color } from "bun";
-import { styleText } from "util";
 import type { InspectColor } from "util";
+import { styleText } from "util";
 
 const colorToBackgroundColor = (colorName: string | null): string | null => {
-  if (!colorName) return null;
-  return `bg${colorName.charAt(0).toUpperCase() + colorName.slice(1)}`;
+	if (!colorName) return null;
+	return `bg${colorName.charAt(0).toUpperCase() + colorName.slice(1)}`;
 };
 
 export const log = (message: string, _color = "white", _bgColor = "") => {
-  const textColor = color(_color, "css");
-  const bgColor = colorToBackgroundColor(color(_bgColor, "css"));
+	const textColor = color(_color, "css");
+	const bgColor = colorToBackgroundColor(color(_bgColor, "css"));
 
-  const styledText = styleText(
-    [textColor, bgColor].filter(Boolean) as InspectColor[],
-    message,
-  );
+	const styledText = styleText(
+		[textColor, bgColor].filter(Boolean) as InspectColor[],
+		message,
+	);
 
-  console.log(styledText);
+	console.log(styledText);
 };
